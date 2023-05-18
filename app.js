a5098a80aa0ea41f59253cc49bc4f67e140824a9
JamesKang003 <kangjames5@gmail.com>
2023-05-18 20:12:49 +0900

    play with variables

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-console.log("Hello from JavaScript!");
-console.log("Let's do some math");
-console.log(4 + 7);
-console.log(12 / 0);
-console.log("Goodbye!");
+// 3 is an expression whose value is 3
+const c = 3;
+// c is an expression whose value is the value of c (3 here)
+let d = c;
+// (d + 1) is an expression whose value is d's + 1 (4 here)
+d = d + 1; // d now contains the value 4
+console.log(d); // Show 4
