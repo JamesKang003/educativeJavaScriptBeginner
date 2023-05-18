67730a52c0b3078ee8dbbd04f95a6c9a7f22ce44
JamesKang003 <kangjames5@gmail.com>
2023-05-18 20:26:37 +0900

    just learned that you can't use prompt or alert in nodejs

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-// 3 is an expression whose value is 3
-const c = 3;
-// c is an expression whose value is the value of c (3 here)
-let d = c;
-// (d + 1) is an expression whose value is d's + 1 (4 here)
-d = d + 1; // d now contains the value 4
-console.log(d); // Show 4
+const input = 3;
+const nb = Number(input); // nb's type is number
+alert(nb);
