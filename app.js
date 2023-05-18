2d4f6a48b9aa32ed9e5527eac781973fe663ecbf
JamesKang003 <kangjames5@gmail.com>
2023-05-18 21:50:34 +0900

    odd counter

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-let number = 1;
-while (number <= 5) {
-  console.log(number);
-  // The number variable is never updated: the loop condition stays true forever
+let n = 1;
+let check = false;
+for (let i = 1; check == false; i++) {
+  if (i % 2 === 1) {
+    console.log(`${i} is odd`);
+    if (n == 10) check = true;
+    n++;
+  }
 }
