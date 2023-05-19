15c5a9e7a213bd3052689f8bde24ff219306e258
JamesKang003 <kangjames5@gmail.com>
2023-05-19 21:16:11 +0900

    loops completed

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 let num = 1;
+
 while (num <= 100) {
-  switch (num) {
-    case num % 3 == 0 && num % 5 == 0:
-      console.log("FizzBuzz");
-    case num % 3 == 0:
-      console.log("Fizz");
-      break;
-    case num % 5 == 0:
-      console.log("Buzz");
-      break;
-    default:
-      console.log(num);
-  }
+  if (num % 3 == 0 && num % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if (num % 3 == 0) {
+    console.log("Fizz");
+  } else if (num % 5 == 0) {
+    console.log("Buzz");
+  } else console.log(num);
+
   num++;
 }
