a0aa856250eb25bf0e7c782a1db991417a36c07b
JamesKang003 <kangjames5@gmail.com>
2023-05-20 15:33:57 +0900

    second min

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-var musketeers = ["Athos", "Porthos", "Aramis"];
-musketeers.push("D' Artagnan");
-musketeers.splice(2, 1);
-console.log(musketeers);
+let input1 = [11, 3, 7, 2, 9, 10];
+let input2 = [-2, -11, -100, 3];
+
+function secondMinimum(values) {
+  min = Number.MAX_SAFE_INTEGER;
+  sndMin = Number.MAX_SAFE_INTEGER;
+  for (value of values) {
+    if (value != min) {
+      if (value < min) {
+        sndMin = min;
+        min = value;
+      } else if (value < sndMin) {
+        sndMin = value;
+      }
+    }
+  }
+  console.log(sndMin);
+}
+secondMinimum(input1);
+secondMinimum(input2);
