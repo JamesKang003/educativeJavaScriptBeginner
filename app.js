dbcefa322d2343be593b68bd6dd65cd892d80a02
JamesKang003 <kangjames5@gmail.com>
2023-05-20 21:59:44 +0900

    I hate html

--- app.js
+++ app.js
@@ -1,57 +1,17 @@
-// Write - Your - Code - Here
-class SocialNewsProgram {
-  constructor() {
-    this.links = [];
+// Show a DOM object's child node
+// "node" is the DOM object
+// "index" is the index of the child node
+//-------
+// For Cases of Wrong node Type , return "Wrong Node Type" String
+// For Cases of Incorrect Index, return "Wrong Index" String
+// For Correct Cases, return the "nodeName" property of that node
+
+function showChild(node, index) {
+  //Write your code here
+  if (!(node instanceof Element)) return "Wrong Node Type";
+  var c = node.children;
+  if (index < 0 || index >= c.length) return "Wrong Index";
+  else {
+    return node.childNodes[index].nodeName;
   }
-  add(link) {
-    this.links.push(link);
-  }
-  remove(index) {
-    this.links.splice(index - 1, 1);
-  }
-  show() {
-    let s = "";
-    for (let i = 0; i < this.links.length; i++) {
-      s += i + 1 + ": " + this.links[i] + "\n";
-    }
-    alert(s);
-  }
-}
-const program = new SocialNewsProgram();
-let input = Number(
-  prompt(
-    "Choose an option: \n0 - Quits\n1 - Show existing links\n2 - Add a link\n3 - Remove a link"
-  )
-);
-while (input != 0) {
-  switch (input) {
-    case 0:
-      break;
-    case 1:
-      program.show();
-      break;
-    case 2:
-      let link = prompt("Enter the link (title, URL and author)");
-      program.add(link);
-      break;
-    case 3:
-      if (program.links.length == 0) {
-        alert("No links to be removed yet");
-      } else if (program.links.length == 1) {
-        program.remove(0);
-      } else {
-        let index = prompt(
-          `Enter the index of the link to be removed (between 1 and ${program.links.length}])`
-        );
-        program.remove(index - 1);
-      }
-      break;
-    default:
-      alert("Invalid input");
-  }
-  input = Number(
-    prompt(
-      "Choose an option: \n0 - Quits\n1 - Show existing links\n2 - Add a link\n3 - Remove a link"
-    )
-  );
 }
