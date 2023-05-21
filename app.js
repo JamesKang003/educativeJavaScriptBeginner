4497244b5a73f2a4e25260d6f503f8d1e3dee369
JamesKang003 <kangjames5@gmail.com>
2023-05-21 17:38:08 +0900

    css

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
-// Show a DOM object's child node
-// "node" is the DOM object
-// "index" is the index of the child node
-//-------
-// For Cases of Wrong node Type , return "Wrong Node Type" String
-// For Cases of Incorrect Index, return "Wrong Index" String
-// For Correct Cases, return the "nodeName" property of that node
-
-function showChild(node, index) {
-  //Write your code here
-  if (!(node instanceof Element)) return "Wrong Node Type";
-  var c = node.children;
-  if (index < 0 || index >= c.length) return "Wrong Index";
-  else {
-    return node.childNodes[index].nodeName;
-  }
-}
+const existsElements = document.getElementsByClassName("exists");
+// All "ancient" wonders that still exist
+console.log(
+  document.getElementById("ancient").getElementsByClassName("exists").length
+); // 1
+console.log(existsElements[0].innerHTML);
